f26895f5d2a921dcc296f9521a9822b23f3c67db
Michał Broniek <s18711@pjwstk.edu.pl>
2020-11-06 17:38:33 +0100

    done

---
+++ Tutorial4/4j.js
@@ -0,0 +1,18 @@
+const binarySearch =  (arr, xToBeFound, left, right) => {
+
+    arr.sort((a, b) => { return a - b});
+
+    if (left > right)
+        return false;
+
+    let mid = Math.floor((left + right)/2);
+
+    if (arr[mid]===xToBeFound) return `index of ${xToBeFound} : ${mid}`;
+
+    if(arr[mid] > xToBeFound)
+        return binarySearch(arr, xToBeFound, left, mid-1);
+    else
+        return binarySearch(arr, xToBeFound, mid+1, right);
+}
+
+console.log(binarySearch([1,2,3,4,5,6,7,9,10],8, 0, 9));
