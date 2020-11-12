c5b02b3ec5e73c9e3414a57495fe4d24b4b92ff7
Michał Broniek <s18711@pjwstk.edu.pl>
2020-11-12 13:24:38 +0100

    all done

--- JsContinued/c.js
+++ JsContinued/c.js
@@ -1,3 +1,17 @@
-function Student(){
-
+function Student(courses)  {
+    this.courses = courses;
 }
+
+const settingUpObject = (firstName, lastName, id) => {
+   Student.prototype.firstName = firstName;
+   Student.prototype.lastName = lastName;
+   Student.prototype.id = id;
+}
+const student1 = new Student(["math","english","bio"]);
+
+settingUpObject("mike","me", 1);
+
+console.log(student1.firstName);
+console.log(student1.lastName);
+console.log(student1.id);
+console.log(student1.courses);
