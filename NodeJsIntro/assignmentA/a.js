de0c1139d62fce44036b8de5b931939211da657c
Michał Broniek <s18711@pjwstk.edu.pl>
2020-11-19 09:35:41 +0100

    assignmentA

--- NodeJsIntro/assignmentA/a.js
+++ NodeJsIntro/assignmentA/a.js
@@ -1,25 +1,57 @@
-const net = require('net');
-const readline = require('readline');
+'use strict';
 
-const client = new net.Socket();
-client.connect(59898, process.argv[2], () => {
-    console.log('Connected to server');
+const http = require('http');
+const url = require('url');
 
+const server = http.createServer((req, res) => {
+
+    const pathname = url.parse(req.url,true).pathname;
+    const number1 = url.parse(req.url,true).query.number1;
+    const number2 = url.parse(req.url,true).query.number2;
+
+    performMathOperation(pathname, number1, number2, res);
 });
-client.on('data', (data) => {
-    console.log(data.toString('utf-8'));
-    if(data.toString('utf-8') === "CLOSE\n"){
-        client.end();
-        console.log("ended");
+
+function performMathOperation(pathname, number1, number2, res) {
+    let sum = 0;
+
+    if (number1 === undefined || number2 === undefined) {
+        res.end('No parameters were passed \nthe request should be in form of  http://127.0.0.1:1337/"operation"?number1="X"&number2="Y"');
+        return;
     }
 
+    const number1Parsed = parseInt(number1);
+    const number2Parsed = parseInt(number2);
+
+    if (isNaN(number1Parsed) || isNaN(number2Parsed)) {
+        res.end('Wrong types, parameters must be of type "number"\n the request should be in form of  http://127.0.0.1:1337/"operation"?number1="X"&number2="Y"');
+        return;
+    }
+
+    switch (pathname) {
+        case "\/add":
+            sum =  number1Parsed + number2Parsed;
+            break;
+
+        case "\/subtract":
+            sum = number1Parsed - number2Parsed;
+            break;
+
+        case "\/multiply":
+            sum = number1Parsed * number2Parsed;
+            break;
+
+        case "\/divide":
+            sum = number1Parsed / number2Parsed;
+            break;
+    }
+    console.log(sum);
+
+    res.end(`the result of your operation is: ${sum}`);
+}
+
+server.listen(1337, "127.0.0.1", () => {
+    console.log(`Server running at http://127.0.0.1:1337/`);
 });
 
-const rl = readline.createInterface({ input: process.stdin });
-rl.on('line', (line) => {
-    client.write(`${line}\n`);
-});
-rl.on('close', () => {
-    client.end();
-    console.log("xd")
-});
+
