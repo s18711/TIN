4ededb3aa8dcddb8c15c8e215db09a18cc2302cf
Michał Broniek <s18711@pjwstk.edu.pl>
2020-11-20 15:22:54 +0100

    assignmentB

--- NodeJsIntro/assignmentB/b.js
+++ NodeJsIntro/assignmentB/b.js
@@ -1,17 +1,23 @@
 const fs = require('fs');
 
-fs.watch('C:\\Users\\hitma\\Desktop\\TIN\\NodeJsIntro\\assignmentB',[true,true,'utf-8'], (eventType, filename) => {
-     // console.log(eventType);
-// could be either 'rename' or 'change'. new file event and delete
-// also generally emit 'rename'
-//     console.log(filename);
-    const file = filename.match("[^~]+");
-    // console.log(file[0]);
-    fs.readFile(`C:\\Users\\hitma\\Desktop\\TIN\\NodeJsIntro\\assignmentB\\${file[0]}`, (err, data) => {
-        if (err) throw err;
-        console.log("Event that caused printing :" + eventType + "\n");
-        console.log("file content: \n")
-        console.log(data.toString());
-    });
+const dir = `C:\\Users\\hitma\\Desktop\\TIN\\NodeJsIntro\\assignmentB\\`;
+
+fs.watch(dir,[true,true,'utf-8'], (eventType, filename) => {
+
+    const fileName = filename.match("[^~]+");
+    const dirAndFile =`${dir}${fileName[0]}`
+
+    try {
+        if (fs.existsSync(dirAndFile)) {
+            fs.readFile(dirAndFile, (err, data) => {
+                if (err) throw err;
+                console.log("Event found :" + eventType + ` in file: ${fileName}\n`);
+                console.log("file content: \n")
+                console.log(data.toString());
+            });
+        }
+    } catch(err) {
+        console.error(err)
+    }
 
 });
