542f75340640532e4c566dbcde59114408abdd5c
MikeV102 <m.broniek72@gmail.com>
2021-01-26 19:42:08 +0100

    adding, deleting and updating works

--- TIN_Project/FrontEnd/front/client/src/CardList.js
+++ TIN_Project/FrontEnd/front/client/src/CardList.js
@@ -1,5 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import BootstrapTable from "react-bootstrap-table-next";
+import {Button, Col, Container, Modal, Row} from 'react-bootstrap';
 import cellEditFactory from 'react-bootstrap-table2-editor';
 
 
@@ -8,16 +9,19 @@ const CardList = (props) => {
     const [items, setItems] = useState([]);
     const [transactions, setTransactions] = useState([]);
     const [selectedRow, setSelectedRow] = useState({});
-
+    const [show, setShow] = useState(false);
+    const [emp, setEmp] = useState({employee_name : '', employee_surname: '', employee_birthday: ''});
     const getData = async () => {
         const jsonResponse = await fetch("http://localhost:9000/getAllRecords").then(response => response.json());
         setEmployees(jsonResponse[0]);
         setItems(jsonResponse[1]);
         setTransactions(jsonResponse[2]);
+        console.log("data received");
     }
 
     useEffect(() => {
         getData();
+        console.log("useEffect");
     }, [])
 
 
@@ -66,7 +70,7 @@ const CardList = (props) => {
             console.log(row);
         }
     }
-    const onSelectHandler = (row,isSelect, rowIndex,e) => {
+    const onSelectHandler = (row, isSelect, rowIndex, e) => {
         setSelectedRow(row)
     }
 
@@ -86,9 +90,10 @@ const CardList = (props) => {
             )
         };
 
-        fetch("http://localhost:9000/updateRecord/employees",updateRequestOptions);
-        console.log("body" , updateRequestOptions.body);
+        fetch("http://localhost:9000/updateRecord/employees", updateRequestOptions);
+
     }
+
 
     const deleteRecord = (event) => {
         const deleteRequestOptions = {
@@ -101,7 +106,7 @@ const CardList = (props) => {
                 }
             )
         };
-        fetch("http://localhost:9000/deleteRecord/employees",deleteRequestOptions);
+        fetch("http://localhost:9000/deleteRecord/employees", deleteRequestOptions);
 
         setEmployees(prevState => {
             const newEmps = prevState.filter((value, index, array) => {
@@ -109,11 +114,45 @@ const CardList = (props) => {
             });
             return newEmps;
         });
-
-
-
+    }
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+    const onChangeHandlerName = (e) => {
+        setEmp(prevState => {
+            return {...prevState, employee_name: e.target.value}
+        });
 
     }
+    const onChangeHandlerSurname = (e) => {
+        setEmp(prevState => {
+            return {...prevState, employee_surname: e.target.value}
+        });
+    }
+    const onChangeHandlerBirthday = (e) => {
+        setEmp(prevState => {
+            return {...prevState, employee_birthday: e.target.value}
+        });
+    }
+
+    const handleSaveEmp = () => {
+        const addRequestOptions = {
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify(
+                {
+                    employee_name: emp.employee_name,
+                    employee_surname: emp.employee_surname,
+                    employee_birthday: emp.employee_birthday
+                }
+            )
+        };
+        fetch("http://localhost:9000/addData/employees", addRequestOptions);
+        setShow(false);
+        getData();
+        getData();
+    }
+
+
     const selectRow = {
         mode: 'radio',
         clickToSelect: true,
@@ -128,19 +167,66 @@ const CardList = (props) => {
         <div className={"container"}>
             <div className={"row"}>
 
-                    <h2>Employees</h2>
-                    <BootstrapTable
-                        rowStyle={{background: 'white'}}
-                        striped={true}
-                        keyField={"id_employee"}
-                        data={employees}
-                        columns={empsColumns}
-                        selectRow={selectRow}
-                        cellEdit={cellEditFactory(cellEdit)}
-                    />
+                <h2>Employees</h2>
+                <BootstrapTable
+                    rowStyle={{background: 'white'}}
+                    striped={true}
+                    keyField={"id_employee"}
+                    data={employees}
+                    columns={empsColumns}
+                    selectRow={selectRow}
+                    cellEdit={cellEditFactory(cellEdit)}
+                />
 
-                    <button type={"button"} className={"btn btn-danger"} onClick={deleteRecord}>Delete</button>
+                <button type={"button"} className={"btn btn-danger"} onClick={deleteRecord}>Delete</button>
+                <button type={"button"} className={"btn btn-primary"} onClick={handleShow}>Add Record</button>
+                <Modal
+                    show={show}
+                    onHide={handleClose}
+                    backdrop="static"
+                    keyboard={false}
 
+                >
+                    <Modal.Header closeButton>
+                        <Modal.Title>Modal title</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <Container>
+                            <Row>
+                                <Col xs={5} md={5}>
+                                    <span className={"modal-label"}>employee_name </span>
+                                </Col>
+                                <Col xs={5} md={4}>
+                                    <input onChange={onChangeHandlerName}/>
+                                </Col>
+                            </Row>
+                            <Row>
+                                <Col xs={5} md={5}>
+                                    <span className={"modal-label"}>employee_surname </span>
+                                </Col>
+                                <Col xs={5} md={4}>
+                                    <input onChange={onChangeHandlerSurname}/>
+                                </Col>
+                            </Row>
+                            <Row>
+                                <Col xs={5} md={5}>
+                                    <span className={"modal-label"}>employee_birthday </span>
+                                </Col>
+                                <Col xs={5} md={4}>
+                                    <input onChange={onChangeHandlerBirthday}/>
+                                </Col>
+                            </Row>
+                        </Container>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="secondary" onClick={handleClose}>
+                            Close
+                        </Button>
+                        <Button variant="primary" onClick={handleSaveEmp}>
+                            Save
+                        </Button>
+                    </Modal.Footer>
+                </Modal>
             </div>
             <div className={"row"}>
                 <h2>Items</h2>
