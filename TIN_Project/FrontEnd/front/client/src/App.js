719462f00bbbb525635353a706a5c03319105f11
MikeV102 <m.broniek72@gmail.com>
2021-01-28 01:24:37 +0100

    added RecordModal.js and DetailsModal.js to make the code cleaner and more reusable

--- TIN_Project/FrontEnd/front/client/src/App.js
+++ TIN_Project/FrontEnd/front/client/src/App.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import CardList from "./CardList";
 import {Container} from 'react-bootstrap';
-import Test from "./Test";
-import Test2 from "./Test2";
+
 
 
 class App extends React.Component {
@@ -14,7 +13,6 @@ class App extends React.Component {
                 <h1>Shop database</h1>
                 <h5>In order to edit, click on table values</h5>
                 <CardList />
-                {/*<Test2/>*/}
             </Container>
         );
     }
