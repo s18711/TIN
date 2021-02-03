2bbbde93ab1b589f0b227a5e017ad13583549671
MikeV102 <m.broniek72@gmail.com>
2021-02-03 17:49:47 +0100

    instructions in Readme.md

--- TIN_Project/FrontEnd/front/client/src/App.js
+++ TIN_Project/FrontEnd/front/client/src/App.js
@@ -12,6 +12,7 @@ class App extends React.Component {
             <Container className={"d-flex flex-column justify-content-center align-items-center "}>
                 <h1>Shop database</h1>
                 <h5>In order to edit, click on table values</h5>
+                <h5>dates should be given in form "YYYY-MM-DD" e.g. 2020-07-30</h5>
                 <CardList />
             </Container>
         );
---
+++ TIN_Project/Readme.md
@@ -0,0 +1,8 @@
+Database setup:
+Copy & execute contents of databaseSETUP.txt in mysql db
+
+Server run:
+in backend/server -> npm start
+
+Client run:
+in FrontEnd/front/client -> npm start
